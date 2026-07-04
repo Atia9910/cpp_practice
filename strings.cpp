15cf2bd5a40a00ffaba3067fcdea69c07b6be1a1
Mohammed Atia <atiamohammed262@gmail.com>
2026-07-04 11:39:24 +0000

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -2,12 +2,13 @@
 #include <string>
 using namespace std;
 
+
 int main(){
     string test = "abcdefghi";
 
-    cout<< "size: "<< test.size() << endl;
+    cout<< "Size: "<< test.size() << endl;
 
-    cout<< "length: "<< test.length() << endl;
+    cout<< "Length: "<< test.length() << endl;
     cout<< "test[1]: "<< test[1]<< endl;
 
     cout<< "test[4]: "<< test.at(4)<< endl;
